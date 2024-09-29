8fd26193c3e7abe8afb20881d03609f41407d180
Conan-Wen <w245646037@gmail.com>
2024-09-29 19:26:25 +0900

    URI修正

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const userController = require('../controllers/userController');
 
 router.post('/signup', userController.signup);
-router.get('/:user_id', userController.getUser);
+router.get('/users/:user_id', userController.getUser);
 router.patch('/:user_id', userController.updateUser);
 router.post('/close', userController.deleteUser);
 
